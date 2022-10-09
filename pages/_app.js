2653e509e38ac036aa456c1383d456cfff34611d
cindy.eke <cindy.eke@telesoftas.com>
2022-10-09 21:25:03 +0100

    added prettier, material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../utils/theme'
 import '../styles/globals.css'
+import createEmotionCache from '../utils/createEmotionCache'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps,
+}) {
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    )
 }
 
 export default MyApp
