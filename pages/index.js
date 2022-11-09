d9cd0213e468d97c177f26411b04bbecf1bb0f0e
cindy.eke <cindy.eke@telesoftas.com>
2022-11-09 06:54:48 +0100

    defined screen support

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,37 @@ import NavProvider from 'src/provider/NavProvider'
 import Services from 'src/sections/Services'
 import Pastors from 'src/sections/Pastors'
 import Contact from 'src/sections/Contact'
+import { useMediaQuery } from 'react-responsive'
+import { Grid, Typography } from '@mui/material'
 
 const App = () => {
+    const isMiniTabletOrMobile = useMediaQuery({ query: '(max-width: 640px)' })
     return (
-        <NavProvider>
-            <Layout>
-                <Home />
-                <About />
-                <Services />
-                <Pastors />
-                <Contact />
-            </Layout>
-        </NavProvider>
+        <>
+            {isMiniTabletOrMobile ? (
+                <NavProvider>
+                    <Layout>
+                        <Home />
+                        <About />
+                        <Services />
+                        <Pastors />
+                        <Contact />
+                    </Layout>
+                </NavProvider>
+            ) : (
+                <Grid
+                    container
+                    xs={12}
+                    justifyContent="center"
+                    alignContent="center"
+                    height="100vh"
+                >
+                    <Typography variant="h1">
+                        This screen is not supported currently
+                    </Typography>
+                </Grid>
+            )}
+        </>
     )
 }
 
